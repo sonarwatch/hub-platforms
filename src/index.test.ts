de93da895e60fd79f1b7da5f593cd6fca7ceb411
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-03-12 15:48:31 +0700

    add zod validation and update platforms with new entries

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,5 @@
 import { platforms } from "./index";
+import { z } from "zod";
 
 describe("Platforms", () => {
   it("should have a length greater than 0", () => {
@@ -7,5 +8,33 @@ describe("Platforms", () => {
 
   it("should have a length less than 5000", () => {
     expect(platforms.length).toBeLessThan(5000);
+  });
+
+  const PlatformSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    image: z.string().url(),
+    links: z.object({
+      website: z.string().url(),
+      discord: z.string().url().optional(),
+      telegram: z.string().url().optional(),
+      twitter: z.string().url().optional(),
+      github: z.string().url().optional(),
+      medium: z.string().url().optional(),
+      documentation: z.string().url().optional(),
+    }),
+  });
+
+  it("should match the PlatformSchema", () => {
+    platforms.forEach((platform) => {
+      expect(() => PlatformSchema.parse(platform)).not.toThrow();
+    });
+  });
+
+  // test for duplicates
+  it("should not have duplicate ids", () => {
+    const ids = platforms.map((platform) => platform.id);
+    const duplicates = ids.filter((id, index) => ids.indexOf(id) !== index);
+    expect(duplicates).toEqual([]);
   });
 });
--- src/platforms.ts
+++ src/platforms.ts
@@ -5,7 +5,14 @@ const platforms: PlatformRaw[] = [
     id: "foo",
     name: "foo platform",
     links: {
-      website: "",
+      website: "https://foo.com",
+    },
+  },
+  {
+    id: "bar",
+    name: "bar platform",
+    links: {
+      website: "https://bar.com",
     },
   },
 ];
