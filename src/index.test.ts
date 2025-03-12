f52b7686a8f2a152e9f2203c05aa2b6d2066729e
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-03-12 15:59:05 +0700

    add fetchPlatforms

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,21 @@
 import { platforms } from "./index";
 import { z } from "zod";
+import { fetchPlatforms } from "./index";
+
+const PlatformSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  image: z.string().url(),
+  links: z.object({
+    website: z.string().url(),
+    discord: z.string().url().optional(),
+    telegram: z.string().url().optional(),
+    twitter: z.string().url().optional(),
+    github: z.string().url().optional(),
+    medium: z.string().url().optional(),
+    documentation: z.string().url().optional(),
+  }),
+});
 
 describe("Platforms", () => {
   it("should have a length greater than 0", () => {
@@ -8,21 +24,6 @@ describe("Platforms", () => {
 
   it("should have a length less than 5000", () => {
     expect(platforms.length).toBeLessThan(5000);
-  });
-
-  const PlatformSchema = z.object({
-    id: z.string(),
-    name: z.string(),
-    image: z.string().url(),
-    links: z.object({
-      website: z.string().url(),
-      discord: z.string().url().optional(),
-      telegram: z.string().url().optional(),
-      twitter: z.string().url().optional(),
-      github: z.string().url().optional(),
-      medium: z.string().url().optional(),
-      documentation: z.string().url().optional(),
-    }),
   });
 
   it("should match the PlatformSchema", () => {
@@ -36,5 +37,12 @@ describe("Platforms", () => {
     const ids = platforms.map((platform) => platform.id);
     const duplicates = ids.filter((id, index) => ids.indexOf(id) !== index);
     expect(duplicates).toEqual([]);
+  });
+});
+
+describe("fetchPlatforms", () => {
+  it("should return an array of platforms", async () => {
+    const result = await fetchPlatforms();
+    expect(Array.isArray(result)).toBeTruthy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,15 @@ const platforms: Platform[] = platformsRaw.map(
   }),
 );
 
+export async function fetchPlatforms(): Promise<Platform[]> {
+  const response = await fetch(
+    "https://github.com/sonarwatch/hub-platforms/releases/latest/download/platforms.json",
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch platforms");
+  }
+  const data: Platform[] = await response.json();
+  return data;
+}
+
 export { platforms };
