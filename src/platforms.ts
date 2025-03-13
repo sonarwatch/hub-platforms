ff6a14904e7aa94dfff4584cbda1067856081c93
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-03-13 12:23:38 +0700

    add zeus

--- src/platforms.ts
+++ src/platforms.ts
@@ -1670,5 +1670,19 @@ const platforms: PlatformRaw[] = [
       twitter: "https://x.com/asgardfi",
     },
   },
+  {
+    id: "zeus",
+    name: "Zeus",
+    links: {
+      website: "https://app.zeusguardian.io/",
+      twitter: "https://x.com/ZeusNetworkHQ",
+      documentation: "https://docs.zeusnetwork.xyz/",
+      github: "https://github.com/ZeusNetworkHQ",
+      discord: "https://discord.com/invite/zeusnetwork",
+      medium: "https://medium.com/@zeus-network",
+    },
+    tokens: ["ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq"],
+    description: "First Multichain Layer on Solana.",
+  },
 ];
 export default platforms;
