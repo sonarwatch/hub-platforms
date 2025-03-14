a7e3348b7d3e32c0629c369115a4ef12c39c6d2b
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-03-14 17:13:39 +0700

    lf

--- src/platforms/index.ts
+++ src/platforms/index.ts
@@ -1,277 +1,1 @@
-import { PlatformRaw } from "../types";
-
-import * as _01 from "./01";
-import * as accessprotocol from "./accessprotocol";
-import * as adrastea from "./adrastea";
-import * as adrena from "./adrena";
-import * as akuma from "./akuma";
-import * as allbridge from "./allbridge";
-import * as armada from "./armada";
-import * as asgardfi from "./asgardfi";
-import * as atrix from "./atrix";
-import * as aurory from "./aurory";
-import * as banx from "./banx";
-import * as baskt from "./baskt";
-import * as blaze from "./blaze";
-import * as bonfida from "./bonfida";
-import * as bonkrewards from "./bonkrewards";
-import * as brawl from "./brawl";
-import * as bskt from "./bskt";
-import * as carrot from "./carrot";
-import * as circuit from "./circuit";
-import * as citrus from "./citrus";
-import * as clone from "./clone";
-import * as coinmarketcap from "./coinmarketcap";
-import * as cropper from "./cropper";
-import * as cyberfrogs from "./cyberfrogs";
-import * as debridge from "./debridge";
-import * as defiland from "./defiland";
-import * as defituna from "./defituna";
-import * as divvy from "./divvy";
-import * as doubleup from "./doubleup";
-import * as drift from "./drift";
-import * as dumpy from "./dumpy";
-import * as elemental from "./elemental";
-import * as ensofi from "./ensofi";
-import * as exponent from "./exponent";
-import * as famousfoxfederation from "./famousfoxfederation";
-import * as flash from "./flash";
-import * as flexlend from "./flexlend";
-import * as flowmatic from "./flowmatic";
-import * as fluxbeam from "./fluxbeam";
-import * as fragmetric from "./fragmetric";
-import * as francium from "./francium";
-import * as frax from "./frax";
-import * as futarchy from "./futarchy";
-import * as gary from "./gary";
-import * as gauntlet from "./gauntlet";
-import * as gecko from "./gecko";
-import * as genesysgo from "./genesysgo";
-import * as gofurs from "./gofurs";
-import * as goosefx from "./goosefx";
-import * as gpool from "./gpool";
-import * as grass from "./grass";
-import * as guano from "./guano";
-import * as hawksight from "./hawksight";
-import * as hedgehog from "./hedgehog";
-import * as hedgy from "./hedgy";
-import * as helium from "./helium";
-import * as hxro from "./hxro";
-import * as iloop from "./iloop";
-import * as jito from "./jito";
-import * as jupiterExchange from "./jupiter-exchange";
-import * as jupiterGovernance from "./jupiter-governance";
-import * as jupiterLaunchpad from "./jupiter-launchpad";
-import * as kamino from "./kamino";
-import * as kishu from "./kishu";
-import * as knightrade from "./knightrade";
-import * as kyros from "./kyros";
-import * as layer3 from "./layer3";
-import * as lifinity from "./lifinity";
-import * as ligma from "./ligma";
-import * as loopscale from "./loopscale";
-import * as m1capital from "./m1capital";
-import * as madbears from "./madbears";
-import * as magiceden from "./magiceden";
-import * as mango from "./mango";
-import * as maple from "./maple";
-import * as marginfi from "./marginfi";
-import * as marinade from "./marinade";
-import * as metaplex from "./metaplex";
-import * as meteora from "./meteora";
-import * as moonwalk from "./moonwalk";
-import * as moose from "./moose";
-import * as neutral from "./neutral";
-import * as nosana from "./nosana";
-import * as nxfinance from "./nxfinance";
-import * as ondoFinance from "./ondo-finance";
-import * as openbook from "./openbook";
-import * as orcaStaking from "./orca-staking";
-import * as orca from "./orca";
-import * as ore from "./ore";
-import * as paraswap from "./paraswap";
-import * as parcl from "./parcl";
-import * as perena from "./perena";
-import * as phoenix from "./phoenix";
-import * as photofinish from "./photofinish";
-import * as picasso from "./picasso";
-import * as pluto from "./pluto";
-import * as port from "./port";
-import * as pudgy from "./pudgy";
-import * as puffcoin from "./puffcoin";
-import * as pyth from "./pyth";
-import * as quarry from "./quarry";
-import * as rain from "./rain";
-import * as ratex from "./ratex";
-import * as raydium from "./raydium";
-import * as realms from "./realms";
-import * as renzo from "./renzo";
-import * as saber from "./saber";
-import * as sanctum from "./sanctum";
-import * as sandglass from "./sandglass";
-import * as save from "./save";
-import * as serum from "./serum";
-import * as sharky from "./sharky";
-import * as solayer from "./solayer";
-import * as sonic from "./sonic";
-import * as spdr from "./spdr";
-import * as stabble from "./stabble";
-import * as stakewise from "./stakewise";
-import * as staratlas from "./staratlas";
-import * as stepfinance from "./stepfinance";
-import * as streamflow from "./streamflow";
-import * as switchboard from "./switchboard";
-import * as symmetry from "./symmetry";
-import * as tensor from "./tensor";
-import * as thevault from "./thevault";
-import * as triad from "./triad";
-import * as tulip from "./tulip";
-import * as uxd from "./uxd";
-import * as vaultka from "./vaultka";
-import * as vectis from "./vectis";
-import * as whalesmarket from "./whalesmarket";
-import * as wormhole from "./wormhole";
-import * as yaku from "./yaku";
-import * as yieldfan from "./yieldfan";
-import * as zelo from "./zelo";
-import * as zeta from "./zeta";
-import * as zeus from "./zeus";
-
-export const platforms: PlatformRaw[] = [
-  _01.platform,
-  accessprotocol.platform,
-  adrastea.platform,
-  adrena.platform,
-  akuma.platform,
-  allbridge.platform,
-  armada.platform,
-  asgardfi.platform,
-  atrix.platform,
-  aurory.platform,
-  banx.platform,
-  baskt.platform,
-  blaze.platform,
-  bonfida.platform,
-  bonkrewards.platform,
-  brawl.platform,
-  bskt.platform,
-  carrot.platform,
-  circuit.platform,
-  citrus.platform,
-  clone.platform,
-  coinmarketcap.platform,
-  cropper.platform,
-  cyberfrogs.platform,
-  debridge.platform,
-  defiland.platform,
-  defituna.platform,
-  divvy.platform,
-  doubleup.platform,
-  drift.platform,
-  dumpy.platform,
-  elemental.platform,
-  ensofi.platform,
-  exponent.platform,
-  famousfoxfederation.platform,
-  flash.platform,
-  flexlend.platform,
-  flowmatic.platform,
-  fluxbeam.platform,
-  fragmetric.platform,
-  francium.platform,
-  frax.platform,
-  futarchy.platform,
-  gary.platform,
-  gauntlet.platform,
-  gecko.platform,
-  genesysgo.platform,
-  gofurs.platform,
-  goosefx.platform,
-  gpool.platform,
-  grass.platform,
-  guano.platform,
-  hawksight.platform,
-  hedgehog.platform,
-  hedgy.platform,
-  helium.platform,
-  hxro.platform,
-  iloop.platform,
-  jito.platform,
-  jupiterExchange.platform,
-  jupiterGovernance.platform,
-  jupiterLaunchpad.platform,
-  kamino.platform,
-  kishu.platform,
-  knightrade.platform,
-  kyros.platform,
-  layer3.platform,
-  lifinity.platform,
-  ligma.platform,
-  loopscale.platform,
-  m1capital.platform,
-  madbears.platform,
-  magiceden.platform,
-  mango.platform,
-  maple.platform,
-  marginfi.platform,
-  marinade.platform,
-  metaplex.platform,
-  meteora.platform,
-  moonwalk.platform,
-  moose.platform,
-  neutral.platform,
-  nosana.platform,
-  nxfinance.platform,
-  ondoFinance.platform,
-  openbook.platform,
-  orcaStaking.platform,
-  orca.platform,
-  ore.platform,
-  paraswap.platform,
-  parcl.platform,
-  perena.platform,
-  phoenix.platform,
-  photofinish.platform,
-  picasso.platform,
-  pluto.platform,
-  port.platform,
-  pudgy.platform,
-  puffcoin.platform,
-  pyth.platform,
-  quarry.platform,
-  rain.platform,
-  ratex.platform,
-  raydium.platform,
-  realms.platform,
-  renzo.platform,
-  saber.platform,
-  sanctum.platform,
-  sandglass.platform,
-  save.platform,
-  serum.platform,
-  sharky.platform,
-  solayer.platform,
-  sonic.platform,
-  spdr.platform,
-  stabble.platform,
-  stakewise.platform,
-  staratlas.platform,
-  stepfinance.platform,
-  streamflow.platform,
-  switchboard.platform,
-  symmetry.platform,
-  tensor.platform,
-  thevault.platform,
-  triad.platform,
-  tulip.platform,
-  uxd.platform,
-  vaultka.platform,
-  vectis.platform,
-  whalesmarket.platform,
-  wormhole.platform,
-  yaku.platform,
-  yieldfan.platform,
-  zelo.platform,
-  zeta.platform,
-  zeus.platform,
-];
+import { PlatformRaw } from "../types";import * as _01 from "./01";import * as accessprotocol from "./accessprotocol";import * as adrastea from "./adrastea";import * as adrena from "./adrena";import * as akuma from "./akuma";import * as allbridge from "./allbridge";import * as armada from "./armada";import * as asgardfi from "./asgardfi";import * as atrix from "./atrix";import * as aurory from "./aurory";import * as banx from "./banx";import * as baskt from "./baskt";import * as blaze from "./blaze";import * as bonfida from "./bonfida";import * as bonkrewards from "./bonkrewards";import * as brawl from "./brawl";import * as bskt from "./bskt";import * as carrot from "./carrot";import * as circuit from "./circuit";import * as citrus from "./citrus";import * as clone from "./clone";import * as coinmarketcap from "./coinmarketcap";import * as cropper from "./cropper";import * as cyberfrogs from "./cyberfrogs";import * as debridge from "./debridge";import * as defiland from "./defiland";import * as defituna from "./defituna";import * as divvy from "./divvy";import * as doubleup from "./doubleup";import * as drift from "./drift";import * as dumpy from "./dumpy";import * as elemental from "./elemental";import * as ensofi from "./ensofi";import * as exponent from "./exponent";import * as famousfoxfederation from "./famousfoxfederation";import * as flash from "./flash";import * as flexlend from "./flexlend";import * as flowmatic from "./flowmatic";import * as fluxbeam from "./fluxbeam";import * as fragmetric from "./fragmetric";import * as francium from "./francium";import * as frax from "./frax";import * as futarchy from "./futarchy";import * as gary from "./gary";import * as gauntlet from "./gauntlet";import * as gecko from "./gecko";import * as genesysgo from "./genesysgo";import * as gofurs from "./gofurs";import * as goosefx from "./goosefx";import * as gpool from "./gpool";import * as grass from "./grass";import * as guano from "./guano";import * as hawksight from "./hawksight";import * as hedgehog from "./hedgehog";import * as hedgy from "./hedgy";import * as helium from "./helium";import * as hxro from "./hxro";import * as iloop from "./iloop";import * as jito from "./jito";import * as jupiterExchange from "./jupiter-exchange";import * as jupiterGovernance from "./jupiter-governance";import * as jupiterLaunchpad from "./jupiter-launchpad";import * as kamino from "./kamino";import * as kishu from "./kishu";import * as knightrade from "./knightrade";import * as kyros from "./kyros";import * as layer3 from "./layer3";import * as lifinity from "./lifinity";import * as ligma from "./ligma";import * as loopscale from "./loopscale";import * as m1capital from "./m1capital";import * as madbears from "./madbears";import * as magiceden from "./magiceden";import * as mango from "./mango";import * as maple from "./maple";import * as marginfi from "./marginfi";import * as marinade from "./marinade";import * as metaplex from "./metaplex";import * as meteora from "./meteora";import * as moonwalk from "./moonwalk";import * as moose from "./moose";import * as neutral from "./neutral";import * as nosana from "./nosana";import * as nxfinance from "./nxfinance";import * as ondoFinance from "./ondo-finance";import * as openbook from "./openbook";import * as orcaStaking from "./orca-staking";import * as orca from "./orca";import * as ore from "./ore";import * as paraswap from "./paraswap";import * as parcl from "./parcl";import * as perena from "./perena";import * as phoenix from "./phoenix";import * as photofinish from "./photofinish";import * as picasso from "./picasso";import * as pluto from "./pluto";import * as port from "./port";import * as pudgy from "./pudgy";import * as puffcoin from "./puffcoin";import * as pyth from "./pyth";import * as quarry from "./quarry";import * as rain from "./rain";import * as ratex from "./ratex";import * as raydium from "./raydium";import * as realms from "./realms";import * as renzo from "./renzo";import * as saber from "./saber";import * as sanctum from "./sanctum";import * as sandglass from "./sandglass";import * as save from "./save";import * as serum from "./serum";import * as sharky from "./sharky";import * as solayer from "./solayer";import * as sonic from "./sonic";import * as spdr from "./spdr";import * as stabble from "./stabble";import * as stakewise from "./stakewise";import * as staratlas from "./staratlas";import * as stepfinance from "./stepfinance";import * as streamflow from "./streamflow";import * as switchboard from "./switchboard";import * as symmetry from "./symmetry";import * as tensor from "./tensor";import * as thevault from "./thevault";import * as triad from "./triad";import * as tulip from "./tulip";import * as uxd from "./uxd";import * as vaultka from "./vaultka";import * as vectis from "./vectis";import * as whalesmarket from "./whalesmarket";import * as wormhole from "./wormhole";import * as yaku from "./yaku";import * as yieldfan from "./yieldfan";import * as zelo from "./zelo";import * as zeta from "./zeta";import * as zeus from "./zeus";export const platforms: PlatformRaw[] = [  _01.platform,  accessprotocol.platform,  adrastea.platform,  adrena.platform,  akuma.platform,  allbridge.platform,  armada.platform,  asgardfi.platform,  atrix.platform,  aurory.platform,  banx.platform,  baskt.platform,  blaze.platform,  bonfida.platform,  bonkrewards.platform,  brawl.platform,  bskt.platform,  carrot.platform,  circuit.platform,  citrus.platform,  clone.platform,  coinmarketcap.platform,  cropper.platform,  cyberfrogs.platform,  debridge.platform,  defiland.platform,  defituna.platform,  divvy.platform,  doubleup.platform,  drift.platform,  dumpy.platform,  elemental.platform,  ensofi.platform,  exponent.platform,  famousfoxfederation.platform,  flash.platform,  flexlend.platform,  flowmatic.platform,  fluxbeam.platform,  fragmetric.platform,  francium.platform,  frax.platform,  futarchy.platform,  gary.platform,  gauntlet.platform,  gecko.platform,  genesysgo.platform,  gofurs.platform,  goosefx.platform,  gpool.platform,  grass.platform,  guano.platform,  hawksight.platform,  hedgehog.platform,  hedgy.platform,  helium.platform,  hxro.platform,  iloop.platform,  jito.platform,  jupiterExchange.platform,  jupiterGovernance.platform,  jupiterLaunchpad.platform,  kamino.platform,  kishu.platform,  knightrade.platform,  kyros.platform,  layer3.platform,  lifinity.platform,  ligma.platform,  loopscale.platform,  m1capital.platform,  madbears.platform,  magiceden.platform,  mango.platform,  maple.platform,  marginfi.platform,  marinade.platform,  metaplex.platform,  meteora.platform,  moonwalk.platform,  moose.platform,  neutral.platform,  nosana.platform,  nxfinance.platform,  ondoFinance.platform,  openbook.platform,  orcaStaking.platform,  orca.platform,  ore.platform,  paraswap.platform,  parcl.platform,  perena.platform,  phoenix.platform,  photofinish.platform,  picasso.platform,  pluto.platform,  port.platform,  pudgy.platform,  puffcoin.platform,  pyth.platform,  quarry.platform,  rain.platform,  ratex.platform,  raydium.platform,  realms.platform,  renzo.platform,  saber.platform,  sanctum.platform,  sandglass.platform,  save.platform,  serum.platform,  sharky.platform,  solayer.platform,  sonic.platform,  spdr.platform,  stabble.platform,  stakewise.platform,  staratlas.platform,  stepfinance.platform,  streamflow.platform,  switchboard.platform,  symmetry.platform,  tensor.platform,  thevault.platform,  triad.platform,  tulip.platform,  uxd.platform,  vaultka.platform,  vectis.platform,  whalesmarket.platform,  wormhole.platform,  yaku.platform,  yieldfan.platform,  zelo.platform,  zeta.platform,  zeus.platform,];
