18f03e0758a34865dee8a91ccdc70b8e862020c8
opptylabs <oppty@proton.me>
2025-04-16 23:11:46 +0200

    add hylo

--- src/platforms/hylo.ts
+++ src/platforms/hylo.ts
@@ -1,12 +1,13 @@
 import { PlatformRaw } from "../types";
 export const platform: PlatformRaw = {
-  id: "texture",
-  name: "Texture",
-  description: "P2P Loans",
+  id: "hylo",
+  name: "Hylo",
+  description: "Stablecoin backed by SOL and leveraged SOL",
   links: {
-    website: "https://texture.finance/",
-    twitter: "https://twitter.com/texture_fi",
-    discord: "https://discord.gg/6YvjpXzsqp",
+    website: "https://hylo.so/",
+    twitter: "https://x.com/hylo_so",
+    telegram: "https://t.me/hylo_announcements",
+    github: "https://github.com/hylo-so",
   },
   tags: ["dapp"],
 };
