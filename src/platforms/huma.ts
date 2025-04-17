5aa384821657fb941200f6738c78de27f81a6f23
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-17 23:01:25 +0200

    add huma

---
+++ src/platforms/huma.ts
@@ -0,0 +1,19 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "huma",
+  name: "Huma",
+  description:
+    "Huma accelerates global payments with instant access to liquidity anywhere, anytime.",
+  tags: ["dapp"],
+  links: {
+    website: "https://huma.finance/",
+    twitter: "https://twitter.com/humafinance",
+    discord: "https://discord.gg/29zVxZ6kCN",
+    github: "https://github.com/00labs",
+    documentation: "https://huma-finance.gitbook.io/huma-2.0",
+  },
+  tokens: [
+    "59obFNBzyTBGowrkif5uK7ojS58vsuWz3ZCvg6tfZAGw",
+    "HUPfpnsaJtJGpJxAPNX1vXah7BgYiQYt1c2JMgMumvPs",
+  ],
+};
