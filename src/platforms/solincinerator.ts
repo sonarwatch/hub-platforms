6868127eb0528a03b52e2a90919089bb0dbdc860
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-24 15:09:39 +0200

    feat: add Sol Incinerator platform with links and description

---
+++ src/platforms/solincinerator.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "solincinerator",
+  name: "Sol Incinerator",
+  description: "Burn any unwanted NFTs or tokens and reclaim the rent",
+  links: {
+    website: "https://sol-incinerator.com/",
+    twitter: "https://x.com/solincinerator",
+    discord: "https://discord.gg/solslugs",
+  },
+  tags: ["tool"],
+};
