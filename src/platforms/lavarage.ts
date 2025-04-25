ad7162d71dd47e3c650e650a1c7a33798a228e4c
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-25 08:13:42 +0200

    feat: add Lavarage platform with details and links

---
+++ src/platforms/lavarage.ts
@@ -0,0 +1,15 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "lavarage",
+  name: "Lavarage",
+  description: "Lavarage is a decentralized leverage trading platform.",
+  defiLlamaId: "lavarage",
+  links: {
+    website: "https://app.lavarage.xyz/",
+    twitter: "https://x.com/lavaragexyz",
+    documentation: "https://lavarage.gitbook.io/",
+    telegram: "https://t.me/lavarage_xyz",
+    medium: "https://medium.com/@lavaragexyz",
+  },
+  tags: ["dapp"],
+};
