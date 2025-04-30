15348786a714f80509364efa42ce1d35d56cc159
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 17:43:47 +0200

    feat: add Backwoods platform configuration

---
+++ src/platforms/backwoods.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "backwoods",
+  name: "Backwoods",
+  description:
+    "A meticulously crafted roguelite game on Solana - fun, rewarding and utterly addictive",
+  links: {
+    website: "https://backwoods.gg/",
+    twitter: "https://x.com/TheBackwoodsSol",
+    discord: "https://discord.gg/backwoodsgg",
+    documentation: "https://backwoods.gg/#",
+  },
+  tags: ["gaming"],
+};
