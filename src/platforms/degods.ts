0552192dcaa672e494588b3e8487d5fbce4a394d
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 18:25:07 +0200

    add degods

---
+++ src/platforms/degods.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "degods",
+  name: "DeGods",
+  links: {
+    twitter: "https://x.com/DeGodsNFT",
+    website: "https://degods.com/",
+    discord: "https://discord.com/invite/dexyz",
+  },
+  tags: ["nft-collection"],
+};
