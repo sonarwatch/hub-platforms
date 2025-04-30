32763f6c32377add011d739453cc4e1d2a79c90e
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 10:16:38 +0200

    add drip

---
+++ src/platforms/drip.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "drip",
+  name: "Drip Haus",
+  description:
+    "The Collectibles Platform. Collect, create, and earn on crypto's #1 content app.",
+  links: {
+    website: "https://drip.haus/",
+    twitter: "https://x.com/drip_haus",
+    documentation: "https://drip.haus/faq",
+  },
+  tags: ["nft-marketplace"],
+};
