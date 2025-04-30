c4b25499d1a16da263441fcab4620636f168b8f5
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 18:41:23 +0200

    add solanart and smb

---
+++ src/platforms/smb.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "smb",
+  name: "Solana Monkey Business",
+  links: {
+    twitter: "https://x.com/MonkeDAO",
+    website: "https://monkedao.io/",
+    discord: "https://discord.gg/monkedao",
+  },
+  tags: ["nft-collection"],
+};
---
+++ src/platforms/solanart.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "solanart",
+  name: "Solanart",
+  links: {
+    website: "https://solanart.io/",
+    twitter: "https://x.com/SolanartNFT",
+  },
+  tags: ["nft-marketplace"],
+  isDeprecated: true,
+};
