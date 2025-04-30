86463fdeea05dad451d4933a28a9e14fe2adf1a3
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 17:30:55 +0200

    add platform configuration for AllDomain

---
+++ src/platforms/alldomain.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "alldomain",
+  name: "AllDomain",
+  description: "Web3 Identity for Everyone",
+  tags: ["tool"],
+  links: {
+    website: "https://alldomains.id/",
+    discord: "https://discord.com/invite/alldomains",
+    telegram: "https://t.me/AllDomainsID",
+    twitter: "https://twitter.com/AllDomains_",
+    github: "https://github.com/onsol-labs",
+    documentation: "https://docs.alldomains.id/protocol/",
+  },
+  tokens: ["BaoawH9p2J8yUK9r5YXQs3hQwmUJgscACjmTkh8rMwYL"],
+};
