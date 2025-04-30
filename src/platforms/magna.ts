acc99be4200c949da4ca5ced2b0d66f346142345
Courmont Guillaume <courmontguillaume@gmail.com>
2025-04-30 19:20:47 +0200

    add magna

---
+++ src/platforms/magna.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "magna",
+  name: "Magna",
+  description:
+    "Manage your token cap table, automate on-or-off chain vesting, account for tax deductions, execute airdrops, distribute conditional grants, utilize dynamic custody solutions and onboard in under a day.",
+  defiLlamaId: "adrastea",
+  tags: ["tool"],
+  links: {
+    website: "https://www.magna.so/",
+    twitter: "https://x.com/magna_digital/photo",
+  },
+};
