ac11cd63788a30e833a3410491ca81ad60db66e6
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-05 12:38:13 +0200

    feat: add Cytonic platform with initial configuration

---
+++ src/platforms/cytonic.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "cytonic",
+  name: "Cytonic",
+  tags: ["bridge", "tool"],
+  links: {
+    website: "https://www.cytonic.com/",
+    twitter: "https://x.com/cytonic_com",
+    discord: "https://discord.com/invite/cytonic",
+    github: "https://github.com/cytonic-network",
+    telegram: "https://t.me/cytonic_official",
+  },
+};
