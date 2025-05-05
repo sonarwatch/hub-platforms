055e9657434e59c7c9048281e4b09213c5bcd2f4
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-05 11:11:30 +0200

    feat: add Boop platform with initial configuration

---
+++ src/platforms/boop.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "boop",
+  name: "Boop",
+  tags: ["memecoin", "launchpad"],
+  links: {
+    website: "https://boop.fun/",
+    twitter: "hhttps://t.me/boopdotfun",
+  },
+  tokens: ["boopkpWqe68MSxLqBGogs8ZbUDN4GXaLhFwNP7mpP1i"],
+};
