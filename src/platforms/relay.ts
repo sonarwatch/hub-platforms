9885a04f9a4c0f5802b5d1e998bc70691c260c1a
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-13 15:26:55 +0200

    add relay and solcasino platform definitions

---
+++ src/platforms/relay.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "relay",
+  name: "Relay",
+  links: {
+    website: "https://relay.link/bridge",
+    twitter: "https://x.com/relayprotocol",
+    documentation: "https://docs.relay.link/what-is-relay",
+  },
+  tags: ["dapp", "bridge"],
+};
---
+++ src/platforms/solcasino.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "solcasino",
+  name: "Solcasino",
+  links: {
+    website: "https://solcasino.io/",
+    twitter: "https://x.com/Solcasinoio",
+    discord: "https://discord.gg/solcasinoio",
+    telegram: "https://t.me/SolcasinoIOBot",
+  },
+  tags: ["gaming"],
+};
