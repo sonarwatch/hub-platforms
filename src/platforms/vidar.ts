268d5279e9a0efa37b749827ea926111c7be608f
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-16 11:44:16 +0200

    feat: add initial platform configuration for Vidar

---
+++ src/platforms/vidar.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "vidar",
+  name: "Vidar",
+  links: {
+    website: "https://www.vidar.finance/dashboard",
+  },
+  tokens: [
+    "K3UfGb8Cr6g2K4GKFEQFxHRhUMq9E9tb3vWepCf1jUp",
+    "7V2ZDLCxZPvx5m5cuexseBHWBMkpj3rfLMuPsuVkecDA",
+  ],
+  tags: ["dapp"],
+};
