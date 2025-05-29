7b27230d5c516a8fa22edc60c28bbf93b1c34fcc
Courmont Guillaume <courmontguillaume@gmail.com>
2025-05-29 15:18:12 +0200

    feat: add platform configuration for Candle

---
+++ src/platforms/candle.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "candle",
+  name: "Candle",
+  links: {
+    website: "https://candle.tv/staking",
+    twitter: "https://x.com/candledottv",
+  },
+  tokens: ["A8bcY1eSenMiMy75vgSnp6ShMfWHRHjeM6JxfM1CNDL"],
+  tags: ["memecoin"],
+};
