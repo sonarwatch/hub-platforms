a72b74d76acc379ccfd30d612f891804519cfe3d
opptylabs <oppty@proton.me>
2025-06-02 17:55:27 +0200

    add spice

---
+++ src/platforms/spice.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "spice",
+  name: "Spice",
+  description:
+    "Token for Governance & Currency in the Lowlife Forms Gameverse.",
+  links: {
+    website: "https://staking.spicecoin.com/",
+    twitter: "https://x.com/spiceonsol",
+  },
+  tags: ["dapp"],
+};
