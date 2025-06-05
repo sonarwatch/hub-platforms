0ea1c83ea0c97c1b55e52f339a5988ff7fa3006f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-06-05 23:41:07 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { platforms as platformsRaw } from "./platforms/index.js";
 import { Platform } from "./types.js";
 
-export { Platform } from "./types.js";
+export { Platform, PlatformTag } from "./types.js";
 export * from "./utils.js";
 
 const platforms: Platform[] = platformsRaw.map(
