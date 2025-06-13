e6e33bebb35a79e18a82dde9b0c850cd97f3bbea
Guillaume <courmontguillaume@gmail.com>
2025-06-13 17:11:50 +0200

    feat: add Wasabi platform integration

---
+++ src/platforms/wasabi.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "wasabi",
+  name: "Wasabi",
+  defiLlamaId: "wasabi",
+  links: {
+    website: "https://app.wasabi.xyz/",
+    discord: "https://discord.gg/wZqzsfnvmh",
+    twitter: "https://x.com/wasabi_protocol",
+    documentation: "https://docs.wasabi.xyz/_/overview/what-is-wasabi",
+  },
+  tags: ["dapp"],
+};
