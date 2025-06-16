3469eaa05698daae3829c21fe78a1d3286cc5e28
Guillaume <courmontguillaume@gmail.com>
2025-06-16 14:24:36 +0200

    feat: add Cushion platform integration

---
+++ src/platforms/cushion.ts
@@ -0,0 +1,10 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "cushion",
+  name: "Cushion",
+  links: {
+    website: "https://app.cushion.trade/vaults",
+    twitter: "https://x.com/cushiondottrade",
+  },
+  tags: ["dapp"],
+};
