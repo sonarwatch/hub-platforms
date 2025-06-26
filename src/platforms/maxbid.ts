70f13af2902a3a03af63ad9d0c969b15e8cf4232
opptylabs <oppty@proton.me>
2025-06-26 09:29:39 +0200

    add maxbid

---
+++ src/platforms/maxbid.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "maxbid",
+  name: "Maxbid Pro",
+  links: {
+    website: "https://maxbid.pro/trade",
+    twitter: "https://x.com/maxbidbro",
+    documentation: "https://maxbid-pro.gitbook.io/maxbid-pro",
+  },
+  tags: ["dapp"],
+};
