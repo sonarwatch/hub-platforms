c38daf9437b12b96b125a22bed1e18312b79a325
Guillaume <courmontguillaume@gmail.com>
2025-06-30 11:35:14 +0200

    feat: add Byreal platform configuration

---
+++ src/platforms/byreal.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "byreal",
+  name: "Byreal",
+  tags: ["dapp"],
+  links: {
+    website: "https://www.byreal.io/",
+    telegram: "https://t.me/Byreal_Community",
+    twitter: "https://x.com/byreal_io",
+    documentation: "https://docs.byreal.io/products/getting-started",
+  },
+};
