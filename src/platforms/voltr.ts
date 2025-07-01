8cbdc6c577d32b27d9c0810f290c60ada6608222
Guillaume <courmontguillaume@gmail.com>
2025-07-01 17:27:30 +0200

    add voltr platform

---
+++ src/platforms/voltr.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "voltr",
+  name: "Voltr",
+  links: {
+    website: "https://voltr.xyz/",
+    twitter: "https://x.com/voltrxyz",
+    github: "https://github.com/voltrxyz",
+    documentation: "https://docs.voltr.xyz/",
+  },
+  tags: ["dapp"],
+};
