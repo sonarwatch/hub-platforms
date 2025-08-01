f14343b194a210ceacac3f90e0d3882cc595cd7c
Courmont Guillaume <courmontguillaume@gmail.com>
2025-08-01 11:59:30 +0200

    add hubble platform

---
+++ src/platforms/hubble.ts
@@ -0,0 +1,18 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "hubble",
+  name: "Hubble",
+  tags: ["dapp"],
+  isDeprecated: true,
+  links: {
+    website: "https://app.hubbleprotocol.io/",
+    twitter: "https://x.com/hubbleprotocol",
+    discord: "https://discord.gg/MGujjftKTY",
+    github: "https://github.com/hubbleprotocol/",
+    documentation: "https://docs.hubbleprotocol.io/",
+  },
+  tokens: [
+    "USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX",
+    "HBB111SCo9jkCejsZfz8Ec8nH7T6THF8KEKSnvwT6XK6",
+  ],
+};
