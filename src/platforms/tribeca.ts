3fcfd7087c780852e17b0b30b006a60d5529dd66
Courmont Guillaume <courmontguillaume@gmail.com>
2025-08-01 10:27:44 +0200

    add tribeca platform

---
+++ src/platforms/tribeca.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "Tribeca",
+  name: "Tribeca",
+  description: "Onchain Governance.",
+  tags: ["dapp", "dao"],
+  links: {
+    website: "https://tribeca.so/gov/",
+    twitter: "https://x.com/thesaberdao",
+    github: "https://github.com/saberdao/tribeca",
+  },
+};
