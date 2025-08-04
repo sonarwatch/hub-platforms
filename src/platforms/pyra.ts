4100458868b2e85fd7f1abf3b83770a2ffc2faa9
opptylabs <oppty@proton.me>
2025-08-04 13:17:13 +0200

    add pyra

---
+++ src/platforms/pyra.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "pyra",
+  name: "Pyra",
+  description: "Credit card and DeFi protocol",
+  tags: ["dapp"],
+  links: {
+    website: "https://www.pyra.fi/",
+    discord: "https://discord.gg/GetPyra",
+    telegram: "https://t.me/GetPyra",
+    twitter: "https://x.com/GetPyra",
+    documentation: "https://docs.pyra.fi/",
+  },
+};
