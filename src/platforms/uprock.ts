0f84d737eac33670501747a0122cc5a4ac399a6a
Courmont Guillaume <courmontguillaume@gmail.com>
2025-08-06 12:15:37 +0200

    feat: add UpRock platform configuration

---
+++ src/platforms/uprock.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "uprock",
+  name: "UpRock",
+  isDeprecated: true,
+  tags: ["depin"],
+  links: {
+    website: "https://uprock.com/",
+    discord: "https://discord.gg/uprock",
+    twitter: "https://twitter.com/uprockcom",
+    github: "https://github.com/uprockcom",
+  },
+  tokens: ["UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG"],
+};
