82d80a33924c875d5679ca8fbae41deaf5100378
opptylabs <oppty@proton.me>
2025-08-07 09:27:50 +0200

    add pancakeswap

---
+++ src/platforms/pancakeswap.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "pancakeswap",
+  name: "PancakeSwap",
+  description: "Trade, earn, and own crypto on the all-in-one multichain DEX.",
+  links: {
+    website: "https://solana.pancakeswap.finance/",
+    twitter: "https://twitter.com/pancakeswap",
+    discord: "https://discord.gg/pancakeswap",
+    github: "https://github.com/pancakeswap/",
+  },
+  tags: ["dex"],
+};
