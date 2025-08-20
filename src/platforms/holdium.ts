bc4fe1b37e001ac5664841af80bb3c227b6f1990
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-08-20 16:00:41 +0800

    add holdium

---
+++ src/platforms/holdium.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "holdium",
+  name: "Holdium",
+  tags: ["memecoin"],
+  isDeprecated: true,
+  links: {
+    website: "https://app.holdium.xyz/",
+    twitter: "https://x.com/holdiumxyz",
+  },
+  tokens: ["3DH8WZ6C2HdDR4J9ekYnTfai8Tx8mxn92mW8zcYmP4px"],
+};
+
+export const services: ServiceRaw[] = [];
