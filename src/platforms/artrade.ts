27a9ff79743f3d9c72cef67d6ff33d85aa90308a
opptylabs <oppty@proton.me>
2025-09-08 13:59:31 +0200

    add artrade

---
+++ src/platforms/artrade.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "artrade",
+  name: "Artrade",
+  description: "Sales club for art & luxury items",
+  links: {
+    website: "https://artrade.app/",
+    twitter: "https://x.com/ArtradeApp",
+    discord: "https://discord.gg/QmCeeqmpMz",
+    telegram: "https://t.me/ArtradeEnglish",
+  },
+  tags: ["dapp"],
+  isDeprecated: true,
+};
+
+export const services: ServiceRaw[] = [];
