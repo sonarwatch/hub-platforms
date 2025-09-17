4b8f81ea8f06aebf3657dee910ac3f4c69c4e77a
Courmont Guillaume <courmontguillaume@gmail.com>
2025-09-17 10:20:47 +0200

    add portals & update switchboard token

---
+++ src/platforms/portals.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "portals",
+  name: "Portals",
+  tags: ["gaming", "nft-collection"],
+  links: {
+    website: "https://theportal.to/",
+    github: "https://github.com/port-finance",
+    twitter: "https://x.com/_portals_",
+    discord: "https://discord.gg/portals",
+  },
+  tokens: ["PRTLSwfLzpVGSAQiUfXEenJkq1cwTsEcsn1hPL9zwwg"],
+};
+export const services: ServiceRaw[] = [];
