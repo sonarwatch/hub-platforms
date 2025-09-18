1dec205a4cbc43a04df2e820a86b367a69b5db44
opptylabs <oppty@proton.me>
2025-09-18 14:25:52 +0200

    add lombard

---
+++ src/platforms/lombard.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "lombard",
+  name: "Lombard",
+  description: "LBTC is an institutional grade yield-bearing BTC.",
+  defiLlamaId: "parent#lombard-finance",
+  tags: ["dapp"],
+  links: {
+    website: "https://www.lombard.finance/",
+    documentation: "https://docs.lombard.finance/",
+    discord: "https://discord.com/invite/2HG7G69twc",
+    twitter: "https://x.com/Lombard_Finance",
+  },
+  tokens: ["LBTCgU4b3wsFKsPwBn1rRZDx5DoFutM6RPiEt1TPDsY"],
+};
+export const services: ServiceRaw[] = [];
