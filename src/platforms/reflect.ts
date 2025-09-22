6d15608df5f9df4a60cda443973786680e736890
opptylabs <oppty@proton.me>
2025-09-22 09:08:23 +0200

    add reflect

---
+++ src/platforms/reflect.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "reflect",
+  name: "Reflect",
+  description: "Autonomous money designed for the stablecoin era",
+  links: {
+    website: "https://www.reflect.money/",
+    twitter: "https://x.com/reflectmoney",
+    documentation: "https://docs.reflect.money/",
+  },
+  tags: ["dapp"],
+};
+export const services: ServiceRaw[] = [];
