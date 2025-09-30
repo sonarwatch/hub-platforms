27e03f3bba172cbdf8c3c0c91600b2a8df5e0cdd
Courmont Guillaume <courmontguillaume@gmail.com>
2025-09-30 15:16:17 +0200

    feat: add Solstice platform configuration

---
+++ src/platforms/solstice.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "solstice",
+  name: "Solstice",
+  tags: ["dapp", "stablecoin"],
+  links: {
+    website: "https://solstice.finance/",
+    twitter: "https://x.com/solsticefi",
+    documentation: "https://docs.solstice.finance/",
+  },
+  tokens: [
+    "6FrrzDk5mQARGc1TDYoyVnSyRdds1t4PbtohCD6p3tgG",
+    "3ThdFZQKM6kRyVGLG48kaPg5TRMhYMKY1iCRa9xop1WC",
+  ],
+};
+export const services: ServiceRaw[] = [];
