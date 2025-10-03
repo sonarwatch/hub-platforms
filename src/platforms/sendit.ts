ba893e70d4fd3ded45a840fdf48350624b6f72ce
opptylabs <oppty@proton.me>
2025-10-03 10:47:38 +0200

    add sendit

---
+++ src/platforms/sendit.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "sendit",
+  name: "Sendit",
+  description: "Permissionless Meme Market",
+  links: {
+    website: "https://sendit.fun/",
+    twitter: "https://x.com/senditfun",
+    discord: "https://discord.com/invite/senditfun",
+    documentation: "https://docs.sendit.fun/",
+  },
+  tags: ["dapp"],
+  isDeprecated: true,
+};
+
+export const services: ServiceRaw[] = [];
