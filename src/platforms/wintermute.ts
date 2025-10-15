374c87f0714877433f8e5559fcf3dd95815c6697
Courmont Guillaume <courmontguillaume@gmail.com>
2025-10-15 18:22:50 +0200

    add wintermute platform

---
+++ src/platforms/wintermute.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "wintermute",
+  name: "Wintermute",
+  links: {
+    website: "https://www.wintermute.com/",
+    twitter: "https://x.com/wintermute_t",
+  },
+  tags: ["liquidity-provider"],
+};
+export const services: ServiceRaw[] = [];
