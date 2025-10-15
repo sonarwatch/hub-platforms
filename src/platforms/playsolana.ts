4b8c6b439b1fff7604b34171bc5389fda30ec961
Courmont Guillaume <courmontguillaume@gmail.com>
2025-10-15 18:46:10 +0200

    add playsolana platform

---
+++ src/platforms/playsolana.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "playsolana",
+  name: "PlaySolana",
+  tags: ["gaming", "dapp"],
+  links: {
+    website: "https://www.playsolana.com/",
+    discord: "https://discord.com/invite/playsolanaofficial",
+    twitter: "https://www.x.com/playsolana",
+  },
+};
+export const services: ServiceRaw[] = [];
