644429c24f41753b90687bbb6ee5708a0d07ac6b
opptylabs <oppty@proton.me>
2025-10-16 17:49:12 +0200

    add claimyoursols

---
+++ src/platforms/claimyoursols.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "claimyoursols",
+  name: "Claim Your Sols",
+  links: {
+    website: "https://claimyoursols.app/",
+  },
+  tags: ["tool"],
+  isDeprecated: true,
+};
+
+export const services: ServiceRaw[] = [];
