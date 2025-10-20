c0a1906f7a27071aaaeab03daed2da763f1c6585
opptylabs <oppty@proton.me>
2025-10-20 15:09:25 +0200

    add oro

---
+++ src/platforms/oro.ts
@@ -0,0 +1,11 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "oro",
+  name: "Oro",
+  links: {
+    website: "https://app.oro.finance/",
+    twitter: "https://x.com/orogoldapp",
+  },
+  tags: ["dapp"],
+};
+export const services: ServiceRaw[] = [];
