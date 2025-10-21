0cca538877a53858649818a02e3830613b0cfcd5
Courmont Guillaume <courmontguillaume@gmail.com>
2025-10-21 13:09:56 +0200

    add project0 platform and services configuration

---
+++ src/platforms/project0.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "project0",
+  name: "Project0",
+  tags: ["dapp"],
+  links: {
+    website: "https://www.0.xyz/",
+    twitter: "https://x.com/0dotxyz",
+    discord: "https://discord.gg/project0",
+  },
+};
+export const services: ServiceRaw[] = [];
