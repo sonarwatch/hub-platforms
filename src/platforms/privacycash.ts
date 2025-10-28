4334ef89455548631b2c85a39cda1c2df65151fe
opptylabs <oppty@proton.me>
2025-10-28 13:39:50 +0100

    add privacycash

---
+++ src/platforms/privacycash.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "privacycash",
+  name: "Privacy Cash",
+  description: "Transfer SOL privately",
+  links: {
+    website: "https://www.privacycash.org/",
+    discord: "https://discord.gg/8guY9uXBvA",
+    twitter: "https://x.com/theprivacycash",
+  },
+  tags: ["dapp"],
+};
+
+export const services: ServiceRaw[] = [];
