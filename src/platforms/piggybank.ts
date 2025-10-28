c6034f827308785604f5c2b6f2a150ae872a6f05
Courmont Guillaume <courmontguillaume@gmail.com>
2025-10-28 12:25:35 +0100

    feat: add Piggybank platform configuration

---
+++ src/platforms/piggybank.ts
@@ -0,0 +1,12 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "piggybank",
+  name: "Piggybank",
+  tags: ["dapp", "lst"],
+  links: {
+    website: "https://www.piggybank.fi/",
+    discord: "https://discord.gg/WNutXfvTgw",
+    twitter: "https://x.com/piggybank_fi",
+  },
+};
+export const services: ServiceRaw[] = [];
