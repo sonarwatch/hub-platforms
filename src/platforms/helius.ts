a015b9f594d64beb87b3e5f2dc0fbd82147c2d0b
opptylabs <oppty@proton.me>
2025-10-31 13:39:16 +0100

    add helius

---
+++ src/platforms/helius.ts
@@ -0,0 +1,13 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "helius",
+  name: "Helius",
+  links: {
+    website: "https://www.helius.dev/",
+    twitter: "https://x.com/heliuslabs",
+    documentation: "https://www.helius.dev/docs",
+  },
+  tags: ["tool"],
+};
+
+export const services: ServiceRaw[] = [];
