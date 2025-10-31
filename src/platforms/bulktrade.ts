5fabe0466a66fc4f5bac0b46a0eb749ad4540ad3
opptylabs <oppty@proton.me>
2025-10-31 11:10:16 +0100

    add bulktrade

---
+++ src/platforms/bulktrade.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "bulktrade",
+  name: "Bulk Trade",
+  tags: ["dapp"],
+  links: {
+    website: "https://www.bulk.trade/",
+    discord: "https://discord.gg/bulk",
+    twitter: "https://x.com/bulktrade",
+    documentation: "https://alphadocs.bulk.trade/",
+  },
+};
+
+export const services: ServiceRaw[] = [];
