3a71e8842e4f0429e329288129d9df83023516e4
opptylabs <oppty@proton.me>
2025-11-21 11:46:18 +0100

    add 1inch

---
+++ src/platforms/1inch.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "1inch",
+  name: "1inch",
+  tags: ["bridge"],
+  links: {
+    website: "https://1inch.com/",
+    discord: "https://discord.com/invite/1inch",
+    twitter: "https://x.com/1inch",
+    github: "https://github.com/1inch",
+  },
+};
+
+export const services: ServiceRaw[] = [];
