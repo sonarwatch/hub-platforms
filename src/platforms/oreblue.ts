d8d7ad2cd60e0164ffa17d2f598fdf14dd4817f9
opptylabs <oppty@proton.me>
2025-11-25 10:42:20 +0100

    add oreblue

---
+++ src/platforms/oreblue.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "oreblue",
+  name: "ORE Blue",
+  tags: ["tool"],
+  links: {
+    website: "https://ore.blue/",
+    discord: "https://discord.com/invite/69uxav4fe4",
+    twitter: "https://x.com/OREdotblue",
+    documentation: "https://ore.blue/about",
+  },
+  tokens: ["orebyr4mDiPDVgnfqvF5xiu5gKnh94Szuz8dqgNqdJn"],
+};
+export const services: ServiceRaw[] = [];
