c4b19b1d9aba4ef1daf850fc8355f1e73113903f
Courmont Guillaume <courmontguillaume@gmail.com>
2025-11-26 10:14:31 +0100

    add solv protocol

---
+++ src/platforms/solv.ts
@@ -0,0 +1,17 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "solv",
+  name: "Solv",
+  tags: ["dapp"],
+  links: {
+    website: "https://solv.finance/",
+    twitter: "https://x.com/SolvProtocol",
+    documentation: "https://docs.solv.finance/",
+    github: "https://github.com/solv-finance",
+  },
+  tokens: [
+    "soLvpPEDkN8D1Wgjezrb1oj4WjGtj17vynGm6t3jah6",
+    "SoLvHDFVstC74Jr9eNLTDoG4goSUsn1RENmjNtFKZvW",
+  ],
+};
+export const services: ServiceRaw[] = [];
