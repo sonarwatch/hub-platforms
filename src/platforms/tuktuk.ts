737de1224c14ac73d6b3e892f94cf37d1856f4aa
opptylabs <oppty@proton.me>
2025-11-27 16:50:51 +0100

    add tuktuk

---
+++ src/platforms/tuktuk.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "tuktuk",
+  name: "TukTuk",
+  description: "Schedule Tasks on Solana",
+  links: {
+    website: "https://www.tuktuk.fun",
+    documentation: "https://www.tuktuk.fun/docs",
+    github: "https://github.com/helium/tuktuk",
+  },
+  tags: ["tool"],
+};
+
+export const services: ServiceRaw[] = [];
