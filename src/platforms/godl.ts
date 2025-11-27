f56cc94df5d4470912aa14a5afee8a8f2da76efe
opptylabs <oppty@proton.me>
2025-11-27 15:48:08 +0100

    add godl

---
+++ src/platforms/godl.ts
@@ -0,0 +1,14 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "godl",
+  name: "GODL",
+  tags: ["tool"],
+  links: {
+    website: "https://www.godl.supply/",
+    discord: "https://discord.com/invite/BDmq4CZXWr",
+    twitter: "https://x.com/GODLsupply",
+    documentation: "https://www.godl.supply/about",
+  },
+  tokens: ["GodL6KZ9uuUoQwELggtVzQkKmU1LfqmDokPibPeDKkhF"],
+};
+export const services: ServiceRaw[] = [];
